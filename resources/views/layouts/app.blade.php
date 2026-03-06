f1b8bd2677f8b07b0a16ff2860fa8abd1e40470e
David Lopez <dimor22@gmail.com>
2026-03-06 13:42:43 -0800

    Logo and favicon added.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ $title ?? 'DailyStars' }}</title>
+    <title>DailyStar</title>
+    <link rel="icon" type="image/png" href="{{ asset('daily-favicon.png') }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
 </head>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,9 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" type="image/png" href="{{ asset('daily-favicon.png') }}">
 
-        <title>Laravel</title>
+        <title>DailyStar</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
