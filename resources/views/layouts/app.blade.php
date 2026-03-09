82aed560ee4e3526b903e4b50bf216c7c939923a
David Lopez <dimor22@gmail.com>
2026-03-09 10:27:59 -0700

    Favicon file name updated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>DailyStars</title>
-    <link rel="icon" type="image/png" href="{{ asset('daily-favicon.png') }}">
+    <link rel="icon" type="image/png" href="{{ asset('dailystars-favicon.png') }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
 </head>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="icon" type="image/png" href="{{ asset('daily-favicon.png') }}">
+        <link rel="icon" type="image/png" href="{{ asset('dailystars-favicon.png') }}">
 
         <title>DailyStars</title>
 
